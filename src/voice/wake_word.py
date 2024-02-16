d1bd5dd4c638f49473b798baedb70dd37e3ad1a6
micartey <me@micartey.dev>
2024-02-16 11:43:17 +0100

    fix: use pvrecorder instead

--- src/voice/wake_word.py
+++ src/voice/wake_word.py
@@ -1,8 +1,7 @@
-import struct
-
 import pvporcupine
-import pyaudio
-from loguru import logger
+
+from pvrecorder import PvRecorder
+from src.audio.record import AudioRecorder
 
 from src.config import (
     PICOVOICE_API_TOKEN,
@@ -11,8 +10,8 @@ from src.config import (
 
 picovoice = pvporcupine.create(
     access_key=PICOVOICE_API_TOKEN,
-    keyword_paths=[f"{WAKE_WORD_FILE}"]
-    # keywords=["ok google"]
+    keyword_paths=[f"{WAKE_WORD_FILE}"],
+    sensitivities=[0.5],
 )
 
 
@@ -22,42 +21,18 @@ def listen_for_wake_word() -> None:
     :return: finishes once word has been detected
     """
 
-    pa = pyaudio.PyAudio()
-    audio_stream = pa.open(
-        rate=picovoice.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=picovoice.frame_length
+    recorder = PvRecorder(
+        frame_length=picovoice.frame_length,
+        device_index=AudioRecorder.selected_device_index,
     )
+    recorder.start()
 
     while True:
-        pcm = audio_stream.read(picovoice.frame_length)
-        pcm = struct.unpack_from("h" * picovoice.frame_length, pcm)
+        pcm = recorder.read()
 
         keyword_index = picovoice.process(pcm)
         if keyword_index >= 0:
             # Wake word detected, abort loop
             break
 
-    audio_stream.stop_stream()
-    audio_stream.close()
-    pa.terminate()
-
-#         audio = whisper.load_audio(stream_file.name)
-#         audio = whisper.pad_or_trim(audio)
-#
-#         mel = whisper.log_mel_spectrogram(audio).to(model.device)
-#         _, probs = model.detect_language(mel)
-#
-#         options = whisper.DecodingOptions(
-#             fp16=False
-#         )  # To make it run on a Raspberry PI
-#         result = whisper.decode(model, mel, options)
-#
-#         logger.debug(f"Detected text ({max(probs, key=probs.get)}): {result.text}")
-#         stream_file.close()
-#
-#         if WAKE_WORD.lower() in result.text.lower():
-#             logger.info("Wake word detected!")
-#             break
+    recorder.delete()
