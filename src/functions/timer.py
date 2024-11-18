662b84aef2166f9d659f3b0d27bbac787a353f59
micartey <me@micartey.dev>
2024-11-18 19:01:35 +0100

    feat: add timer sound

--- src/functions/timer.py
+++ src/functions/timer.py
@@ -4,11 +4,12 @@ from time import sleep
 
 from loguru import logger
 
+from src.audio.play import play_mp3
 from src.config import ee
 
 
 @ee.on("timer")
-def handle_alarm(json_arg: str) -> None:
+def handle_timer(json_arg: str) -> None:
     logger.info("Execute function: Timer")
 
     seconds = json.loads(json_arg)["seconds"]
@@ -20,4 +21,6 @@ def handle_alarm(json_arg: str) -> None:
 def after_seconds(delay: int):
     sleep(delay)
     # TODO: Play sound
+
+    play_mp3("assets/sounds/timer_finished.mp3")
     logger.debug("Timer finished!")
